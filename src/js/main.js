4deb41994c76c2451c261ac6244108a8b0c4383a
Matthew Craig <taggedzi@users.noreply.github.com>
2022-04-01 18:56:35 -0400

    Working event timer

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 require("flatpickr/dist/themes/dark.css")
 import flatpickr from "flatpickr"
 import '../css/main.css'
+import '../css/flip.css'
+import '../js/flip.js'
 
 const tzEventTimer = () => {
     flatpickr('#newEvent', {
@@ -22,28 +24,59 @@ function constructUrl(queryParameters = {}){
 }
 
 const newEventButton = document.getElementById('create')
-const clientOffset = document.getElementById('clientOffset')
 newEventButton.addEventListener('click', function(event) {
     const eventTimeEl = document.getElementById('newEvent')
     if (eventTimeEl.value !== '') {
         let eventUtcTime = getEventTime(eventTimeEl.value)
+        let eventDescription = document.getElementById('newEventDesc').value
         let queryParams = {
-            'date': eventUtcTime
+            'date': eventUtcTime,
+            'desc': eventDescription
         }
         let url = constructUrl(queryParams)
-        clientOffset.value = url
+        document.getElementById('link').innerHTML = `<a href="${url}">${url}</a>`
     } else {
         throw new Error('Must select a date/time for an event.')
     }
 })
 
 const queryString = window.location.search;
-const currentEventEl = document.getElementById('existingEvent')
 if (queryString) {
     let searchParams = new URLSearchParams(queryString)
+
     let date = ''
+
     if (searchParams.has('date')) {
         date = new Date(searchParams.get('date'))
     }
-    currentEventEl.value = date
+
+    document.getElementById('existingEventHeader').innerHTML = 'Currently Scheduled Event'
+    document.getElementById('existingEventTime').innerHTML = date
+    document.getElementById('eventDesc').innerText = searchParams.get('desc')
+
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    const countDown = new Date(date).getTime(),
+        x = setInterval(function () {
+
+            const now = new Date().getTime(),
+                distance = countDown - now;
+
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+            //do something later when date is reached
+            if (distance < 0) {
+                document.getElementById("headline").innerText = "Event start has passed.";
+                document.getElementById("countdown").style.display = "none";
+                document.getElementById("content").style.display = "block";
+                clearInterval(x);
+            }
+            //seconds
+        }, 0)
 }
