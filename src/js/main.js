ebceffb5015f7ea66128c13dc2e47aaf6b5757ca
Matthew Craig <taggedzi@users.noreply.github.com>
2022-04-01 17:19:07 -0400

    Working event timer input

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
-require("flatpickr/dist/themes/dark.css");
-import flatpickr from "flatpickr";
+require("flatpickr/dist/themes/dark.css")
+import flatpickr from "flatpickr"
+import '../css/main.css'
 
 const tzEventTimer = () => {
     flatpickr('#newEvent', {
@@ -9,3 +10,35 @@ const tzEventTimer = () => {
 };
 tzEventTimer();
 
+
+function getEventTime(localTimeString) {
+    return new Date(localTimeString).toUTCString();
+}
+
+
+function constructUrl(queryParameters = {}){
+    let sp = new URLSearchParams(queryParameters);
+    const uri = `${location.protocol}//${location.host}:${location.port}${location.pathname}?${sp.toString()}`
+    return uri
+}
+
+const newEventButton = document.getElementById('create')
+newEventButton.addEventListener('click', function(event) {
+    const eventTimeEl = document.getElementById('newEvent')
+    if (eventTimeEl.value !== '') {
+        let eventUtcTime = getEventTime(eventTimeEl.value)
+        let queryParams = {
+            'date': eventUtcTime
+        }
+        let url = constructUrl(queryParams)
+
+        console.log({
+            eventUtcTime,
+            queryParams,
+            url
+        })
+        // clientOffset.value = utcTime.toUTCString()
+    } else {
+        throw new Error('Must select a date/time for an event.')
+    }
+})
